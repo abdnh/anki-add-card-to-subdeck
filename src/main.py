2f5dcc1b7649a3bce82a3aa95ed19078e381c23e
Abdo <abdo@abdnh.net>
2023-10-13 08:10:53 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os
+import sys
+
+from aqt import gui_hooks, mw
+from aqt.addcards import AddCards
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "vendor"))
+
+from .log import logger
+
+
+def on_add_cards_did_init(addcards: AddCards) -> None:
+    card = mw.reviewer and mw.reviewer.card
+    if not card:
+        return
+    new_deck_id = card.current_deck_id()
+    if new_deck_id != addcards.deck_chooser.selected_deck_id:
+        logger.debug(
+            "changing addcards deck from %s to %s",
+            addcards.deck_chooser.selected_deck_id,
+            new_deck_id,
+        )
+        addcards.deck_chooser.selected_deck_id = new_deck_id
+
+
+gui_hooks.add_cards_did_init.append(on_add_cards_did_init)
