968590cf1223336e101bc11fcd66265a88d854a6
Abdo <abdo@abdnh.net>
2023-11-07 23:26:12 +0300

    Remove log

--- src/main.py
+++ src/main.py
@@ -6,8 +6,6 @@ from aqt.addcards import AddCards
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "vendor"))
 
-from .log import logger
-
 
 def on_add_cards_did_init(addcards: AddCards) -> None:
     card = mw.reviewer and mw.reviewer.card
@@ -15,11 +13,6 @@ def on_add_cards_did_init(addcards: AddCards) -> None:
         return
     new_deck_id = card.current_deck_id()
     if new_deck_id != addcards.deck_chooser.selected_deck_id:
-        logger.debug(
-            "changing addcards deck from %s to %s",
-            addcards.deck_chooser.selected_deck_id,
-            new_deck_id,
-        )
         addcards.deck_chooser.selected_deck_id = new_deck_id
 
 
